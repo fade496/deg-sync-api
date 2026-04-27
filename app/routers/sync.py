75a6055b62c0a90611be1a66430b59ef39c7c0df
fade496 <151085318+fade496@users.noreply.github.com>
2026-04-27 13:10:24 -0600

    Update sync.py

--- app/routers/sync.py
+++ app/routers/sync.py
@@ -7,6 +7,7 @@ from app.services.sync_projects import sync_projects
 from app.services.sync_people import sync_people
 from app.services.sync_tasks import sync_tasks
 from app.services.sync_project_people import sync_project_people
+from app.services.sync_project_tasks import sync_project_tasks
 
 router = APIRouter(prefix="/sync", tags=["sync"])
 
@@ -58,3 +59,11 @@ def sync_project_people_route(
 ):
     check_key(x_api_key=x_api_key, authorization=authorization)
     return sync_project_people()
+
+@router.post("/project-tasks")
+def sync_project_tasks_route(
+    x_api_key: str = Header(None),
+    authorization: str = Header(None),
+):
+    check_key(x_api_key=x_api_key, authorization=authorization)
+    return sync_project_tasks()
