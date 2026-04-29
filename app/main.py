ec1a06bbc1d25852b0b5eea3d253959d9e838188
fade496 <151085318+fade496@users.noreply.github.com>
2026-04-29 13:02:55 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,33 +1,8 @@
 from fastapi import FastAPI
 
-from app.routers import (
-    oauth,
-    status,
-    query,
-    sync,
-    create,
-    update,
-    test,
-    airtable_generic,
-    scheduler,
-    lem,
-)
-
 app = FastAPI(title="DEG Sync API")
 
 
 @app.get("/")
 def root():
-    return {"message": "DEG Sync API running"}
-
-
-app.include_router(oauth.router)
-app.include_router(status.router)
-app.include_router(query.router)
-app.include_router(sync.router)
-app.include_router(create.router)
-app.include_router(update.router)
-app.include_router(test.router)
-app.include_router(airtable_generic.router)
-app.include_router(scheduler.router)
-app.include_router(lem.router)
+    return {"message": "DEG Sync API running from app.main"}
